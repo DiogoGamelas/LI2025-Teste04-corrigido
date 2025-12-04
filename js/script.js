61d0a7753fa7a03575d5e71e8958bdd111d25a11
DiogoGamelas <diogo.gamelas@ua.pt>
2025-12-04 15:34:22 +0000

    5ª tarefa cumprida

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-//ficheiro de script
+// Exemplo de JavaScript inicial para desativar envios de formulários se houver campos inválidos
+(() => {
+  'use strict'
+
+  // Selecionar todos os formulários que queremos aplicar estilos de validação Bootstrap
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Fazer um loop sobre eles e impedir o envio
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
